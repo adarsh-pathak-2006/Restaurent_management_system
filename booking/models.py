6aba91428052cbb9debc8155add54d3dd7aaea5a
duckyduck01 <madhvetipathak2006@gmail.com>
2026-08-16 15:40:22 +0530

    added models in the booking app too

--- booking/models.py
+++ booking/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Table(models.Model):
+    table_id=models.CharField(max_length=3)
+    seats=models.ValueRange(start=2, end=4)
+    is_avaliable=models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.table_id
+
+
+class Reservation(models.Model):
+    user=models.ForeignKey(User, on_delete=models.CASCADE)
+    table=models.OneToOneField(Table, on_delete=models.CASCADE)
+    created_at=models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.table.table_id} -- {self.id}"
+
