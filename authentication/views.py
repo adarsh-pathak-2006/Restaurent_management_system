6d8d136aefca8b8aa78c3d828391d621fc9708eb
duckyduck01 <madhvetipathak2006@gmail.com>
2026-08-16 17:05:00 +0530

    added registration

--- authentication/views.py
+++ authentication/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from django.conf import settings
+from .serializers import RegisterSerializer
+from django.db.models import Q
+from rest_framework.response import Response
+from django.contrib.auth import get_user_model
 
-# Create your views here.
+User=get_user_model()
+
+class RegisterAPI(APIView):
+    def post(self, request):
+        serial=RegisterSerializer(data=request.data)
+        if serial.is_valid():
+            username=serial.validated_data['username']
+            email=serial.validated_data['email']
+            role=serial.validated_data['role']
+            password=serial.validated_data['password']
+
+            if User.objects.filter(Q(username=username) | Q(email=email)).exists():
+                return Response({'message':'username or email already exists'}, status=400)
+            User.objects.create_user(username=username, email=email, role=role, password=password)
+            return Response({'message':'user registration successfull'}, status=201)
+        return Response(serial.errors, status=400)
--- config/settings.py
+++ config/settings.py
@@ -115,6 +115,7 @@ USE_I18N = True
 
 USE_TZ = True
 
+AUTH_USER_MODEL="authentication.User"
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
